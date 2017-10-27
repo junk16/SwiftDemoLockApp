e5fd4bdabf80558124d57ead172bf76990f2355b
jun yamada <jun.16@mac.com>
2017-10-27 21:35:46 +0900

    update lockfunction

--- LockDemoApp/ViewController.swift
+++ LockDemoApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet var password: UITextField!
     
+    private let authorizeKey = "ZGVtbw=="
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,8 +29,20 @@ class ViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-    @IBAction func authroze(_ sender: UIButton) {
-        let dialog = UIAlertController(title: "test", message: password.text,
+    @IBAction func lockScreen(_ sender: UIButton) {
+        let decodedText = password.text!.data(using: .utf8)!.base64EncodedString()
+        
+        var title   = ""
+        var message = ""
+        if decodedText == authorizeKey {
+            title   = "認証成功"
+            message = "画面をロックします。\n（DEMOなの機能未実装）"
+        }else{
+            title   = "認証失敗"
+            message = "パスワードが間違っています。"
+        }
+        
+        let dialog = UIAlertController(title: title, message: message,
                                        preferredStyle: UIAlertControllerStyle.alert)
         let button = UIAlertAction(title: "OK"
             , style: UIAlertActionStyle.cancel, handler: nil)
