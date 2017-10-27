b58dbcc4b6c09e29234206978c7fa3af45a7dd21
jun yamada <jun.16@mac.com>
2017-10-27 13:24:51 +0900

    first commit

--- LockDemoApp/ViewController.swift
+++ LockDemoApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,18 @@ class ViewController: UIViewController {
     }
 
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func authroze(_ sender: UIButton) {
+        let dialog = UIAlertController(title: "test", message: "demo", preferredStyle: UIAlertControllerStyle.alert)
+        let button = UIAlertAction(title: password.text
+            , style: UIAlertActionStyle.cancel, handler: nil)
+        
+        dialog.addAction(button)
+        
+        present(dialog, animated: true, completion: nil)
+    }
 }
 
